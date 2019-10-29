58ba427e9d63b21126c98c1dc5af44b7d0be1bfc
Timothy Sharkey <Techne3@gmail.com>
2019-10-29 14:01:22 -0600

    Delete post by specified ID

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -24,10 +24,10 @@ router.get('/:id', (req,res) => {
     db.findById(id) 
     .then(item => {
         console.log(item)
-        if(id){
-            res.status(200).json(item)
-        }else{
+        if(item.length === 0){
             res.status(400).json({message: 'The post with the specified ID does not exist'})
+        }else{
+            res.status(200).json(item)
         }
     })
     .catch(err => {
@@ -35,6 +35,23 @@ router.get('/:id', (req,res) => {
     })
 });
 
+
+// delete post with specific ID
+router.delete(`/:id`, (req,res ) => {
+    const id = req.params.id
+
+    db.remove(id)
+    .then(del => {
+        if(!del){
+            res.status(404).json({message: 'The post wit the specified ID does not exist'})
+        }else {
+            res.status(200).json({message: `Post with ID ${id} was successfully deleted`})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error: "The post could not be removed"})
+    })
+})
 
 
 module.exports =router;
